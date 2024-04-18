c65cc937ac441cc45fbb0a78b7f3234932698edb
zinkjm <zinkjm@mail.uc.edu>
2024-04-17 21:43:02 -0400

    Fixing Loan date bug. It works

--- screens/DueBooksScreen.js
+++ screens/DueBooksScreen.js
@@ -6,7 +6,7 @@ const DueBooksScreen = () => {
   const { books, updateBook } = useContext(BookContext); // Access books array and updateBook function from context
 
   // Filter books with a loan date
-  const dueBooks = books.filter((book) => book.loan !== null);
+  const dueBooks = books.filter((book) => book.loan);
 
   // Sort books by loan date in ascending order
   dueBooks.sort((a, b) => new Date(a.loan) - new Date(b.loan));
